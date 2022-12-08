50adbf8372c515923f922505ecafccb772d90e2d
Kishan Kumar <krishankumar9943@gmail.com>
2022-12-08 21:02:08 +0530

    SubInfo Complete

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+};
